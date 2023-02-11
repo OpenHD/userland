f3d5bdfbae73df8c8ffd76a93e3e9a87d777303b
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-02-11 20:20:48 +0100

    change the service remove the script

--- omx_video.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtph264depay ! 'video/x-h264,stream-format=byte-stream' ! fdsink | hello_video.bin
